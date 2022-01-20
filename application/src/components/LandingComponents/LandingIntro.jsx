2778ab5783e8dd722621a49387117dd33f2b453c
Mosta Eltazy <mostafa.eltazy@levelset.com>
2022-01-20 21:24:57 +0200

    Landing Intro placeholder

--- application/src/components/LandingComponents/LandingIntro.jsx
+++ application/src/components/LandingComponents/LandingIntro.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const LandingIntro = () => {
-  return <div className="col-md-5">Landing Intro</div>;
+  return <section className="col-md-3 d-flex flex-column align-self-start mt-5 mb-4">
+    <h2 className="mb-4">
+      Welcome ..
+    </h2>
+    <article>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique laborum exercitationem assumenda laboriosam neque aperiam architecto corrupti. Corrupti dolores quod similique porro doloribus quibusdam ex. Qui atque magnam accusamus eum. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, possimus minima! Numquam ratione consectetur cumque doloribus aliquam totam, recusandae et ea ad alias, reiciendis veniam esse explicabo repudiandae quod fugit!</article>
+    </section>;
 };
 
 export default LandingIntro;
